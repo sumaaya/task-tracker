18f8705d7a8b286e2f185c37cf22da24912fe708
Wayne <waynejotham@gmail.com>
2025-05-20 16:46:40 +0300

    chore(firebase): Firebase files

--- Task/src/app/app.config.ts
+++ Task/src/app/app.config.ts
@@ -4,7 +4,7 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { environment } from '../environments/environment.development';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }),
